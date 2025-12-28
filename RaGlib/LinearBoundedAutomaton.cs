0f9e9e4bef73f4ec557ef74f94bc24e312daca81
OlyaSilaeva-dev <olya-dom2014@yandex.ru>
2025-12-28 19:54:20 +0300

    gitignore

--- RaGlib/LinearBoundedAutomaton.cs
+++ RaGlib/LinearBoundedAutomaton.cs
@@ -13,26 +13,101 @@ namespace RaGlib
 
         public static LBA FromCSG(CSGrammar grammar)
         {
-            var terminals = grammar.Terminals.Select(t => t.symbol).ToList();
-            var nonterminals = grammar.NonTerminals.Select(nt => nt.symbol).ToList();
+            var T = grammar.Terminals.Select(t => t.symbol).ToList();
+            var N = grammar.NonTerminals.Select(nt => nt.symbol).ToList();
             var productions = grammar.CSProductions;
 
             // Положим V = V_T  ∪ V_N и пусть
             // Σ содержит состояния t0, t1, s0, s1, r0, r1,
             // а также sA для каждого A такого,
             // что существует правило вида AB → CD.
-            var V = terminals.Union(nonterminals).ToList();
-            var states = new List<string>{"t0", "t1", "s0", "s1", "r0", "r1"};
+            var V = T.Union(N).ToList();
+            var states = new List<string> { "t0", "t1", "s0", "s1", "r0", "r1" };
 
             foreach (var p in productions)
             {
-                foreach (var t in terminals) 
+                foreach (var t in T)
                 {
-                    if (p.LHS.Contains(t)) {
+                    if (p.LHS.Contains(t))
+                    {
                         states.Add("s" + t);
                     }
                 }
             }
+
+            var sigma = new List<string>(V) { "$" };
+            var lba = new LBA(states, sigma, new List<string> { "halt" }, "t0");
+
+            string S = grammar.StartSymbol.symbol;
+
+            //(#,t_0) → (#,t_1,1)
+            lba.AddRule("t0", "$", "$", Direction.Right, "t1");
+
+            //(a,t_1) → (a,t_1,1) для всех a в V_T
+            foreach (var t in T)
+            {
+                lba.AddRule("t1", "a", "a", Direction.Right, "t1");
+            }
+
+            //(#,t_1) → (#,s_0,-1)
+            lba.AddRule("t1", "$", "$", Direction.Left, "s0");
+
+            //(A,s_0) → (A,s_0,1) для всех A в V
+            //(A,s_0) → (A,s_0,-1) для всех A в V
+            foreach (var A in V)
+            {
+                lba.AddRule("s0", A, A, Direction.Right, "s0");
+                lba.AddRule("s0", A, A, Direction.Left, "s0");
+            }
+
+            //(B,s_0) → (A,s_0,0) для всех правил A → B в G
+            //(C,s_0) → (A,s_A,1) для всех правил AB → CD в G
+            //(D,s_A) → (B,s_0,0) для всех правил AB → CD в G
+            //(S,s_0) → (S,r_0,-1) для всех правил S → SA  в G
+
+            //(S,r_1) → (#,s_1,1),
+            //(A, s_1) → (S, s_0, 0),
+            foreach (var p in productions)
+            {
+                var rhs = p.RHS;
+                var lhs = p.LHS;
+
+                if (lhs.Count == 1 && rhs.Count == 1)
+                {
+                    lba.AddRule("s0", rhs[0].symbol, lhs[0].symbol, Direction.Stay, "s0");
+                }
+
+                if (p.LHS.Count == 2 && p.RHS.Count == 2)
+                {
+                    var A = lhs[0].symbol;
+                    var B = lhs[1].symbol;
+                    var C = rhs[0].symbol;
+                    var D = rhs[1].symbol;
+
+                    var sA = "s" + A;
+                    lba.AddRule("s0", C, A, Direction.Right, sA);
+                    lba.AddRule(sA, D, B, Direction.Stay, "s0");
+                }
+
+
+                if (lhs.Count == 1 && rhs.Count == 2 && (lhs[0] == rhs[0]))
+                {
+                    var A = rhs[1].symbol;
+                    lba.AddRule("s0", S, S, Direction.Left, "r0");
+                    lba.AddRule("s1", A, S, Direction.Left, "s0");
+                }
+
+                lba.AddRule("r1", S, "$", Direction.Right, "s1");
+
+            }
+
+            //(#,r_0) → (#,r_1,1)
+            lba.AddRule("r0", "$", "$", Direction.Right, "r1");
+
+            //(#,s_1) → (#,t_0,1)
+            lba.AddRule("s1", "$", "$", Direction.Right, "t0");
+
+
             /*var sigma = new List<string>(V) { "$" };
 
             var states = new List<string>
@@ -115,9 +190,8 @@ namespace RaGlib
                         lba.AddRule(st, sym, sym, Direction.Stay, "reject");
                 }
             }
-
-            return lba;*/
-            throw new Exception();
+            */
+            return lba;
         }
     }
 }
